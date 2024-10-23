8f8123e85973854dc416f369586adbe8131bb56b
Angelo Vellar <angelovellar24@gmail.com>
2024-10-23 19:59:12 -0300

    Update: Agregando controller y service de Booking

---
+++ src/main/java/DevGroup/BookingReyunos/repository/BookingRepository.java
@@ -0,0 +1,8 @@
+package DevGroup.BookingReyunos.repository;
+
+import DevGroup.BookingReyunos.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking,Integer> {
+
+}
---
+++ src/main/java/DevGroup/BookingReyunos/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package DevGroup.BookingReyunos.repository;
+
+import DevGroup.BookingReyunos.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review,Integer> {
+}
